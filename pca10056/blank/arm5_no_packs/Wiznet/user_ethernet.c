23b057d34c4d4fa1ead48629ce9f70a5f9240997
Alireza Mohammadi <alireza17010@gmail.com>
2019-03-11 14:25:51 +0330

    Update user_ethernet.c

--- pca10056/blank/arm5_no_packs/Wiznet/user_ethernet.c
+++ pca10056/blank/arm5_no_packs/Wiznet/user_ethernet.c
@@ -54,15 +54,6 @@ void user_ethernet_init(){
       while(1);
     }
 		
-		//////// Not used for w5100 ///////
-    // PHY link status check
-    //printf("W5100 PHY Link Status Check\r\n");
-		/*do
-    {
-       if(ctlwizchip(CW_GET_PHYLINK, (void*)&tmp) == -1)
-    	   printf("Shit\r\n");
-    }while(tmp == PHY_LINK_OFF);*/
-		///////////////////////////////////
 
     timeout_info.retry_cnt = 1;
     timeout_info.time_100us = 0x3E8;	// timeout value = 10ms
@@ -78,13 +69,5 @@ void network_init(){
 	ctlnetwork(CN_SET_NETINFO, (void*)&gWIZNETINFO);
 	ctlnetwork(CN_GET_NETINFO, (void*)&gWIZNETINFO);
 
-	// Display Network Information
 	ctlwizchip(CW_GET_ID,(void*)tmpstr);
-//	printf("\r\n=== %s NET CONF ===\r\n",(char*)tmpstr);
-//	printf("MAC: %02X:%02X:%02X:%02X:%02X:%02X\r\n",gWIZNETINFO.mac[0],gWIZNETINFO.mac[1],gWIZNETINFO.mac[2], gWIZNETINFO.mac[3],gWIZNETINFO.mac[4],gWIZNETINFO.mac[5]);
-//	printf("SIP: %d.%d.%d.%d\r\n", gWIZNETINFO.ip[0],gWIZNETINFO.ip[1],gWIZNETINFO.ip[2],gWIZNETINFO.ip[3]);
-//	printf("GAR: %d.%d.%d.%d\r\n", gWIZNETINFO.gw[0],gWIZNETINFO.gw[1],gWIZNETINFO.gw[2],gWIZNETINFO.gw[3]);
-//	printf("SUB: %d.%d.%d.%d\r\n", gWIZNETINFO.sn[0],gWIZNETINFO.sn[1],gWIZNETINFO.sn[2],gWIZNETINFO.sn[3]);
-//	printf("DNS: %d.%d.%d.%d\r\n", gWIZNETINFO.dns[0],gWIZNETINFO.dns[1],gWIZNETINFO.dns[2],gWIZNETINFO.dns[3]);
-//	printf("======================\r\n");
 }
